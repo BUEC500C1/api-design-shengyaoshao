019ccb8e4cf7a361ea34df31bb38440502fb7737
shengyaoshao <55214118+shengyaoshao@users.noreply.github.com>
2020-02-09 19:48:46 -0500

    Update tweetimage.py

--- tweetimage.py
+++ tweetimage.py
@@ -5,21 +5,36 @@ import urllib.request
 from google.cloud import vision
 from google.cloud.vision import types
 
-consumer_key = 'sdaw'
-consumer_secret = 'wqer'
-access_key = 'acxc'
-access_secret = 'saqwe'
+consumer_key = 'asdasd'
+consumer_secret = 'asdasd'
+access_key = 'asdasd'
+access_secret = 'asdasd'
 
-def get_all_tweets(account):
+def get_all_tweets():
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
         api = tweepy.API(auth)
         alltweets = []
-        new_tweets = api.user_timeline(screen_name = account,count=1)
+        print("enter the name of the twitter account")
+        account = input()
+        print("How many tweets you would like to scan, you can scan as many as 200")
+        number = input()
+        while (int(number) > 200):
+                print('The number you entered is biger than 200, please enter a number less than 200')
+                number = input()
+        try:
+                new_tweets = api.user_timeline(screen_name = account,count=1)
+        except:
+                print('This is not a valid Twitter account')
+                get_all_tweets()
         alltweets.extend(new_tweets)
-        oldest = alltweets[-1].id - 1
+        try:
+                oldest = alltweets[-1].id - 1
+        except:
+                print('This account does not have this many tweets')
+                get_all_tweets()
         tweetnum = 1
-        while(tweetnum < 10):
+        while(tweetnum <= int(number)):
                 new_tweets = api.user_timeline(screen_name = account, count=1, max_id = oldest)
                 alltweets.extend(new_tweets)
                 tweetnum += 1
@@ -34,17 +49,21 @@ def get_all_tweets(account):
                         urllib.request.urlretrieve(url,name)
                         fileorder += 1
                 except (NameError, KeyError):
-
+                       
                         pass
                 else:
-
+                        
                         outtweets.append([tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"), tweet.entities['media'][0]['media_url']])
+        imagenum = fileorder - 1
+        print(str(imagenum)+'images detected')
         return fileorder
 def googlevision(f):
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'TweetImage-ef41c01d2f34.json'
         client = vision.ImageAnnotatorClient()
         path = r'C:\Users\Vanquish\Desktop\pyve\VisionApi\downloadimage'
         file_name = 1
+        if(f == 1):
+                print("No image posted by this account in the recent 10 tweets")
         while(file_name < f):
                 with io.open(os.path.join(path, str(file_name) + '.jpg'),'rb') as image_file:
                         content = image_file.read()
@@ -60,23 +79,35 @@ def googlevision(f):
                                 '{}\nFor more info on error messages, check: '
                                 'https://cloud.google.com/apis/design/errors'.format(
                                 response.error.message))
-                response = client.image_properties(image=image)
-                props = response.image_properties_annotation
-                print('Properties of image' + str(file_name) + ':')
 
-                for color in props.dominant_colors.colors:
-                        print('fraction: {}'.format(color.pixel_fraction))
-                        print('\tr: {}'.format(color.color.red))
-                        print('\tg: {}'.format(color.color.green))
-                        print('\tb: {}'.format(color.color.blue))
-                        print('\ta: {}'.format(color.color.alpha))
+                response = client.face_detection(image=image)
+                faces = response.face_annotations
+                # Names of likelihood from google.cloud.vision.enums
+                likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+                print('Faces:')
 
+                for face in faces:
+                        if (likelihood_name[face.anger_likelihood] == 'VERY_LIKELY'):
+                                print('This person is very likely to be angery')
+                        elif (likelihood_name[face.anger_likelihood] == 'LIKELY'):
+                                print('This person is likely to be angery')
+                        elif (likelihood_name[face.joy_likelihood] == 'VERY_LIKELY'):
+                                print('This person is very likely to be happy')
+                        elif (likelihood_name[face.joy_likelihood] == 'LIKELY'):
+                                print('This person is likely to be happy')
+                        elif (likelihood_name[face.surprise_likelihood] == 'VERY_LIKELY'):
+                                print('This person is very likely to be surprised')
+                        elif (likelihood_name[face.surprise_likelihood] == 'LIKELY'):
+                                print('This person is likely to be surprised')
+                        else:
+                                print('Unable to determine')
                 if response.error.message:
                         raise Exception(
                                 '{}\nFor more info on error messages, check: '
                                 'https://cloud.google.com/apis/design/errors'.format(
-                                response.error.message))
+                                        response.error.message))
                 file_name += 1
 if __name__ == '__main__':
-        f = get_all_tweets('@realDonaldTrump')
-        googlevision(f) 
+        f = get_all_tweets()
+        googlevision(f)
